4adc8baf40b5892e9b1fd2b96753760f71e1a847
Seitek2002 <seitek_2002@bk.ru>
2024-05-15 18:43:39 +0600

    first commit

--- index.js
+++ index.js
@@ -1,65 +1,112 @@
-$('.single-product__slider--mobile').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dots: true,
-    mobileFirst: true,
-})
+$(".single-product__slider").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1022,
+      settings: {
+        arrows: true,
+      },
+    },
+  ],
+});
 
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav',
-})
-$('.slider-nav').slick({
-    slidesToShow: $('.slider-nav div').length,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    focusOnSelect: true,
-    arrows: false,
-    vertical: true,
-    verticalSwiping: true,
-    adaptiveHeight: true,
-    variableWidth: true,
-    infinite: false,
-})
+$(".hero__slider").slick({
+  arrows: false,
+  dots: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1022,
+      settings: {
+        arrows: true,
+      },
+    },
+  ],
+});
 
-const selectItems = document.querySelectorAll('.select-item')
+$(".review__slider").slick({
+  arrows: false,
+  dots: true,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 1022,
+      settings: {
+        arrows: true,
+      },
+    },
+  ],
+});
 
-selectItems.forEach(item => {
-    item.addEventListener('click', () => {
-        selectItems.forEach(el => el.classList.remove('active'))
+const selectItems = document.querySelectorAll(".select-item");
 
-        item.classList.add('active')
-    })
-})
+selectItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    selectItems.forEach((el) => el.classList.remove("active"));
 
-const accordeons = document.querySelectorAll('.accordeon')
+    item.classList.add("active");
+  });
+});
 
-accordeons.forEach(item => {
-    item.querySelector('.accordeon__label').addEventListener('click', () => {
-        item.classList.toggle('active')
-    })
-})
+const accordeons = document.querySelectorAll(".accordeon");
 
-const selectBox = document.querySelectorAll('.select-box')
+accordeons.forEach((item) => {
+  item.querySelector(".accordeon__label").addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
 
-selectBox.forEach(box => {
-    box.querySelectorAll('.select-box__item').forEach(item => {
-        item.addEventListener('click', () => {
-            box.querySelectorAll('.select-box__item').forEach(item => item.classList.remove('active'))
-            item.classList.add('active')
-        })
-    })
-})
+const selectBox = document.querySelectorAll(".select-box");
 
-const tooltip = document.querySelector('span.product_label-tooltip-span')
+selectBox.forEach((box) => {
+  box.querySelectorAll(".select-box__item").forEach((item) => {
+    item.addEventListener("click", () => {
+      box
+        .querySelectorAll(".select-box__item")
+        .forEach((item) => item.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+});
 
-tooltip.addEventListener('click', e => {
-    e.stopPropagation()
-    document.querySelector('.product_label-tooltip.product_label-tooltip--visible').classList.toggle('active')
-})
+const tooltip = document.querySelector("span.product_label-tooltip-span");
+
+tooltip.addEventListener("click", (e) => {
+  e.stopPropagation();
+  document
+    .querySelector(".product_label-tooltip.product_label-tooltip--visible")
+    .classList.toggle("active");
+});
+
+function openModal() {
+  // document.getElementById('myModal').style.display = 'block'
+  // document.body.style.overflow = 'hidden'
+}
+
+function closeModal() {
+  // document.getElementById('myModal').style.display = 'none'
+  // document.body.style.overflow = 'auto'
+}
+
+const counter = document.getElementById("counter");
+const span = counter.querySelector(".span");
+const minusButton = counter.querySelector(".minus");
+const plusButton = counter.querySelector(".plus");
+
+let count = 0;
+span.textContent = count;
+
+minusButton.addEventListener("click", () => {
+  count--;
+  span.textContent = count < 0 ? 0 : count; 
+});
+
+plusButton.addEventListener("click", () => {
+  count++;
+  span.textContent = count;
+});
